6117d44e79e7c202c53b72b79f594da10c074e2f
Cristian Romero <cristianrom66@gmail.com>
2025-05-04 20:43:34 -0500

    FIX: dead code removed

--- apps/backend/src/services/charactersService.ts
+++ apps/backend/src/services/charactersService.ts
@@ -35,25 +35,3 @@ export class CharacterService {
     return Object.keys(where).length > 0 ? await Character.findAll({ where }) : await this.getAllCharacters()
   }
 }
-
-// export const getAllCharacters = async () => {
-//   const characters = await Character.findAll()
-//   return characters
-// }
-
-// export const filteredCharacters = async (filters: FilteredCharacterProps) => {
-//   const safeFilters = filters || {}
-
-//   const where = Object.entries(safeFilters)
-//     .filter(([_, value]) => value)
-//     .reduce(
-//       (acc: Record<string, any>, [key, value]) => {
-//         acc[key] = key === 'name' ? { $like: `%${value}%` } : value
-//         return acc
-//       },
-//       {} as Record<string, any>,
-//     )
-
-//   const characters = Object.keys(where).length > 0 ? await Character.findAll({ where }) : await getAllCharacters()
-//   return characters
-// }
